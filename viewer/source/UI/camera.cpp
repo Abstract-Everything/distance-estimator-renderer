ccdb651ffe3947726b9f24c807e001b05bb6fc15
Jonathan Camilleri <jon.d.cam@gmail.com>
2020-10-11 18:58:46 +0100

    [viewer] Pan camera slower and reduce zoom delta.
    Panning was too fast so it was slowed down for better control.
    
    A small turn of the mouse wheel zoomed too much, the zooming mechanism
    was tuned to zoom a smaller amount each time.

--- viewer/source/UI/camera.cpp
+++ viewer/source/UI/camera.cpp
@@ -15,6 +15,8 @@ Camera::Camera() : QQuickItem()
 		&Singletons::Renderer::update_shader,
 		this,
 		&Camera::shader_updated);
+
+	reset_move_direction();
 }
 
 void Camera::initialise_signals()
@@ -52,10 +54,7 @@ void Camera::shader_updated()
 	{
 		qDebug() << "Camera position size is neither 2 nor 3";
 	}
-
-	move_direction.setX (0);
-	move_direction.setY (0);
-	move_direction.setZ (0);
+	reset_move_direction();
 }
 
 bool Camera::eventFilter (QObject* watched, QEvent* event)
@@ -68,6 +67,7 @@ bool Camera::eventFilter (QObject* watched, QEvent* event)
 
 	case QEvent::HoverLeave:
 		watched->setProperty ("focus", false);
+		reset_move_direction();
 		return true;
 
 	case QEvent::KeyPress:
@@ -102,10 +102,40 @@ float Camera::get_zoom()
 	return zoom.value (0).toFloat();
 }
 
+void Camera::set_zoom(float zoom_value)
+{
+	zoom_value = qBound (
+		-1.0f + cnst::min_ui_float,
+		zoom_value,
+		1.0f - cnst::min_ui_float);
+	Uniform zoom = Singletons::renderer.get_uniform ("camera.zoom");
+	zoom.set_value (zoom_value, 0);
+	Singletons::renderer.set_uniform (zoom);
+}
+
 float Camera::get_zoom_factor()
 {
 	const float zoom = get_zoom();
-	return qBound (0.0f, zoom + 1.0f, 2.0f);
+	return qBound (cnst::min_ui_float, zoom * -1.0f + 1.0f, 2.0f);
+}
+
+std::tuple<QVector3D, QVector3D, QVector3D> Camera::get_basis()
+{
+	const float pitch
+		= Singletons::renderer.get_uniform ("camera.pitch").value (0).toFloat();
+	const float yaw
+		= Singletons::renderer.get_uniform ("camera.yaw").value (0).toFloat();
+
+	
+	QVector3D forward
+		= QVector3D (qSin (yaw), qSin (pitch), qCos (yaw)).normalized();
+
+	QVector3D right
+		= QVector3D::crossProduct (QVector3D (0.0f, 1.0f, 0.0f), forward)
+			  .normalized();
+
+	QVector3D up = QVector3D::crossProduct(forward, right).normalized();
+	return {right, up, forward};
 }
 
 void Camera::update_uniforms()
@@ -119,61 +149,83 @@ void Camera::update_uniforms()
 
 void Camera::update_zoom(float delta_time)
 {
-	if (qAbs (zoom_offset) < cnst::min_ui_float)
+	const float zoom_delta_remaining = qAbs(get_zoom() - zoom_before);
+	const float zoom_delta           = zoom_after - zoom_before;
+	const float zoom_delta_abs       = qAbs (zoom_delta);
+	if (qAbs (zoom_delta_remaining - zoom_delta_abs)
+		< zoom_delta_abs / cnst::zoom_factor + cnst::min_ui_float)
 	{
 		return;
 	}
+	const float direction = zoom_delta / zoom_delta_abs;
 
-	Uniform zoom = Singletons::renderer.get_uniform ("camera.zoom");
-	const float zoom_value  = zoom.value (0).toFloat();
-	const float zoom_change = zoom_delta * delta_time;
-	zoom.set_value (qBound (-1.0f, zoom_value + zoom_change, 1.0f), 0);
-	Singletons::renderer.set_uniform (zoom);
-	zoom_offset -= zoom_change;
-	if (qAbs (zoom_offset) < qAbs (zoom_change))
-	{
-		zoom_offset = 0.0f;
-	}
+	const float zoom_change
+		= direction
+		  * qMax (zoom_delta_abs * delta_time * 2.0f, cnst::min_ui_float);
+	set_zoom (get_zoom() + zoom_change);
 }
 
 void Camera::update_position (float delta_time)
 {
-	const QVector3D move_offset
-		= move_direction * delta_time * get_zoom_factor();
+	const float multiplier = delta_time * get_zoom_factor();
 	Uniform position = Singletons::renderer.get_uniform ("camera.position");
 	if (dimensions == Dimensions::Two)
 	{
-		update_position_2d (move_offset, position);
+		update_position_2d (multiplier, position);
 	}
 	else if (dimensions == Dimensions::Three)
 	{
-		update_position_3d (move_offset, position);
+		update_position_3d (multiplier, position);
 	}
 	Singletons::renderer.set_uniform (position);
 }
 
-void Camera::update_position_2d (
-	QVector3D const& move_offset,
-	Uniform& position)
+void Camera::update_position_2d (float multiplier, Uniform& position)
 {
+	const QVector2D move_offset (
+		determine_move_direction (Qt::Key_D, Qt::Key_A) * multiplier,
+		determine_move_direction (Qt::Key_W, Qt::Key_S) * multiplier);
+	
 	position.set_value (position.value (0).toFloat() + move_offset.x(), 0);
 	position.set_value (position.value (1).toFloat() + move_offset.y(), 1);
 }
 
-void Camera::update_position_3d (
-	QVector3D const& move_offset,
-	Uniform&         position)
+void Camera::update_position_3d (float multiplier, Uniform& position)
 {
-	position.set_value (position.value (0).toFloat() + move_offset.x(), 0);
-	position.set_value (position.value (1).toFloat() + move_offset.y(), 1);
-	position.set_value (position.value (2).toFloat() + move_offset.z(), 2);
+	const QVector3D move_offset (
+		determine_move_direction (Qt::Key_D, Qt::Key_A) * multiplier,
+		determine_move_direction (Qt::Key_Space, Qt::Key_Control) * multiplier,
+		determine_move_direction (Qt::Key_W, Qt::Key_S) * multiplier);
+
+	auto [right, up, forward] = get_basis();
+	QVector3D position_value  = move_offset.x() * right + move_offset.y() * up
+							   + move_offset.z() * forward;
+	
+	position.set_value (position.value (0).toFloat() + position_value.x(), 0);
+	position.set_value (position.value (1).toFloat() + position_value.y(), 1);
+	position.set_value (position.value (2).toFloat() + position_value.z(), 2);
+}
+
+float Camera::determine_move_direction (Qt::Key forward, Qt::Key backwards)
+{
+	float direction = 0.0f;
+	if (move_keys_pressed[forward])
+	{
+		direction += 1.0f;
+	}
+
+	if (move_keys_pressed[backwards])
+	{
+		direction -= 1.0f;
+	}
+	return direction * move_speed;
 }
 
 void Camera::update_view (float /* delta_time */)
 {
 	const QVector2D offset
 		= get_zoom_factor() * QVector2D (-1.0f, 1.0f) * pan_direction;
-	pan_direction          = {0.0f, 0.0f};
+	pan_direction = {0.0f, 0.0f};
 	if (dimensions == Dimensions::Two)
 	{
 		update_view_2d (offset);
@@ -186,107 +238,62 @@ void Camera::update_view (float /* delta_time */)
 
 void Camera::update_view_2d(QVector2D const& offset)
 {
+	const float aspect
+		= static_cast<float> (height()) / static_cast<float> (width());
+	QVector2D view_offset = QVector2D (offset.x(), offset.y() * aspect)
+							/ cnst::screen_in_pixels_2d;
+
 	Uniform position = Singletons::renderer.get_uniform ("camera.position");
-	position.set_value (position.value (0).toFloat() + offset.x(), 0);
-	position.set_value (position.value (1).toFloat() + offset.y(), 1);
+	position.set_value (position.value (0).toFloat() + view_offset.x(), 0);
+	position.set_value (position.value (1).toFloat() + view_offset.y(), 1);
 	Singletons::renderer.set_uniform (position);
 }
 
 void Camera::update_view_3d (QVector2D const& offset)
 {
+	QVector2D view_offset
+		= QVector2D (offset.x() / width(), offset.y() / height()) * cnst::pi_2;
+
 	Uniform yaw       = Singletons::renderer.get_uniform ("camera.yaw");
-	float yaw_value = (yaw.value (0).toFloat() + offset.x() * cnst::pi);
+	float   yaw_value = (yaw.value (0).toFloat() + view_offset.x());
 	yaw.set_value (yaw_value, 0);
 	Singletons::renderer.set_uniform (yaw);
 
 	Uniform pitch = Singletons::renderer.get_uniform ("camera.pitch");
 	const float pitch_value = qBound (
 		-cnst::pi_2,
-		pitch.value (0).toFloat() + offset.y() * cnst::pi,
+		pitch.value (0).toFloat() + view_offset.y(),
 		cnst::pi_2);
 	pitch.set_value (pitch_value, 0);
 	Singletons::renderer.set_uniform (pitch);
 }
 
+void Camera::reset_move_direction()
+{
+	move_keys_pressed[Qt::Key_A] = false;
+	move_keys_pressed[Qt::Key_D] = false;
+	move_keys_pressed[Qt::Key_Space] = false;
+	move_keys_pressed[Qt::Key_Control] = false;
+	move_keys_pressed[Qt::Key_W] = false;
+	move_keys_pressed[Qt::Key_S] = false;
+}
+
 void Camera::update_move_direction (QKeyEvent const& key_event)
 {
-	if (dimensions == Dimensions::Two)
-	{
-		update_move_direction_2d (key_event);
-	}
-	else if (dimensions == Dimensions::Three)
-	{
-		update_move_direction_3d (key_event);
-	}
-}
-
-void Camera::update_move_direction_2d (QKeyEvent const& key_event)
-{
-	if (key_event.type() == QEvent::KeyPress)
-	{
-		switch (key_event.key())
-		{
-		case Qt::Key_A: move_direction.setX (-1); break;
-		case Qt::Key_D: move_direction.setX (1); break;
-		case Qt::Key_W: move_direction.setY (1); break;
-		case Qt::Key_S: move_direction.setY (-1); break;
-		}
-	}
-	else if (key_event.type() == QEvent::KeyRelease)
-	{
-		switch (key_event.key())
-		{
-		case Qt::Key_A:
-		case Qt::Key_D: move_direction.setX (0); break;
-
-		case Qt::Key_W:
-		case Qt::Key_S: move_direction.setY (0); break;
-		}
-	}
-}
-
-void Camera::update_move_direction_3d (QKeyEvent const& key_event)
-{
-	if (key_event.type() == QEvent::KeyPress)
-	{
-		switch (key_event.key())
-		{
-		case Qt::Key_A:       move_direction.setX (-1); break;
-		case Qt::Key_D:       move_direction.setX (1); break;
-		case Qt::Key_Space:   move_direction.setY (1); break;
-		case Qt::Key_Control: move_direction.setY (-1); break;
-		case Qt::Key_W:       move_direction.setZ (1); break;
-		case Qt::Key_S:       move_direction.setZ (-1); break;
-		}
-	}
-	else if (key_event.type() == QEvent::KeyRelease)
-	{
-		switch (key_event.key())
-		{
-		case Qt::Key_A:
-		case Qt::Key_D: move_direction.setX (0); break;
-
-		case Qt::Key_Space:
-		case Qt::Key_Control: move_direction.setY (0); break;
-
-		case Qt::Key_W:
-		case Qt::Key_S: move_direction.setZ (0); break;
-		}
-	}
+	Qt::Key key            = static_cast<Qt::Key> (key_event.key());
+	move_keys_pressed[key] = (key_event.type() == QEvent::KeyPress);
 }
 
 void Camera::update_pan_direction (QMouseEvent const& mouse_event)
 {
-	QVector2D mouse_position{
-		static_cast<float> (mouse_event.pos().x() / width()),
-		static_cast<float> (mouse_event.pos().y() / height())};
+	QVector2D mouse_position{mouse_event.pos()};
 
 	switch (mouse_event.type())
 	{
 	case QEvent::MouseButtonPress: last_mouse_position = mouse_position; break;
 
 	case QEvent::MouseMove:
-		pan_direction       = QVector2D (mouse_position - last_mouse_position);
+		pan_direction       += QVector2D (mouse_position - last_mouse_position);
 		last_mouse_position = mouse_position;
 		break;
 
@@ -298,15 +305,18 @@ void Camera::update_pan_direction (QMouseEvent const& mouse_event)
 
 void Camera::update_zoom_offset (QWheelEvent const& wheel_event)
 {
-	if (qAbs (wheel_event.angleDelta().y()) == 0)
+	const float offset = wheel_event.angleDelta().y();
+	if (qAbs (offset) == 0)
 	{
 		return;
 	}
-	const float offset    = wheel_event.angleDelta().y();
-	const float direction = offset / qAbs (offset);
+	const float direction  = offset / qAbs (offset);
+	const float remaining  = 1.0f - qAbs (zoom_after);
+	const float zoom_delta = direction * remaining / cnst::zoom_factor;
 
-	const float zoom        = qAbs (get_zoom() + zoom_offset);
-	const float zoom_change = 1.0f - zoom;
-	zoom_offset             = direction * qMax (zoom_change, cnst::min_ui_float);
-	zoom_delta              = zoom_offset;
+	zoom_before = get_zoom();
+	zoom_after  = qBound (
+        -1.0f + cnst::min_ui_float,
+        zoom_after + zoom_delta,
+        1.0f - cnst::min_ui_float);
 }
--- viewer/source/constants.hpp
+++ viewer/source/constants.hpp
@@ -6,4 +6,6 @@ const float pi   = qAcos (-1);
 const float pi_2 = pi / 2.0f;
 
 constexpr float min_ui_float = 0.000001f;
+constexpr float screen_in_pixels_2d = 512.0f;
+constexpr float zoom_factor         = 5.0f;
 } // namespace
